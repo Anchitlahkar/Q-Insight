946908852f735103e117fbf225edbd8cdea1c88a
Anchit Lahkar <64302945+Anchitlahkar@users.noreply.github.com>
2026-03-31 00:53:34 +0530

    feat: add @vercel/analytics dependency and integrate analytics component in layout

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,4 +1,5 @@
 ﻿import type { Metadata } from "next";
+import { Analytics } from "@vercel/analytics/next"
 import "./globals.css";
 
 export const metadata: Metadata = {
@@ -19,6 +20,7 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <body suppressHydrationWarning>{children}</body>
+      <Analytics />
     </html>
   );
 }
