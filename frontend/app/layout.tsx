6b8ba1c65420b9434526c28fd5ed64452f843cba
Anchit Lahkar <64302945+Anchitlahkar@users.noreply.github.com>
2026-03-31 00:57:03 +0530

    feat: integrate @vercel/analytics for enhanced tracking and update layout structure

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,5 +1,5 @@
 ﻿import type { Metadata } from "next";
-import { Analytics } from "@vercel/analytics/next"
+import { Analytics } from "@vercel/analytics/next";
 import "./globals.css";
 
 export const metadata: Metadata = {
@@ -19,8 +19,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body suppressHydrationWarning>{children}</body>
-      <Analytics />
+      <body suppressHydrationWarning>
+        {children}
+        <Analytics />
+      </body>
     </html>
   );
 }
